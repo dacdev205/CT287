2c4f6d2b709de03c81b37368215fd47d2baef475
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-03-28 19:15:33 +0700

    add more cases

--- test.py
+++ test.py
@@ -3,16 +3,15 @@ import re
 from unidecode import unidecode
 
 # Đọc dữ liệu từ tập tin Excel
-
 df = pd.read_excel("Vung-Khuvuc.xlsx")
 
 # Loại bỏ khoảng trắng ở cuối mỗi giá trị trong cột "Huyện, Tỉnh"
 df["Huyện, Tỉnh"] = df["Huyện, Tỉnh"].str.strip()
 
 # Đọc dữ liệu từ tệp văn bản
-with open('Vung.txt', 'r', encoding='utf-16') as f:
+with open('Vung.txt', 'r', encoding='utf-16') as f:  # Sửa encoding thành utf-8
     vung_data = f.read()
-
+vung_data = vung_data.replace("-", ",")
 # Tạo một danh sách chứa tên vùng và tỉnh thành từ file Vung.txt
 regions_mapping = {}
 for match in re.finditer(r"Vùng\s([^:]+)\s.*?gồm\s(\d+\s)?tỉnh(?: thành)?:\s(.*?)\.", vung_data):
@@ -26,17 +25,16 @@ def check_province_name(province_name):
     if isinstance(province_name, str):  # Kiểm tra nếu là một chuỗi
         # Kiểm tra xem chuỗi có chứa bất kỳ tỉnh thành nào trong danh sách không
         for province, region in regions_mapping.items():
-            # Chuyển đổi các ký tự có dấu thành ký tự không dấu trước khi so sánh
-            province_normalized = unidecode(province)
-            province_name_normalized = unidecode(province_name)
-            # Tạo một biểu thức chính quy để tìm kiếm tên tỉnh thành trong chuỗi
-            pattern = r"\b{}\b".format(province_normalized.replace(" ", r"[\s-]*"))
+            # Giữ nguyên chuỗi tỉnh thành, chỉ loại bỏ khoảng trắng ở đầu và cuối
+            province_normalized =  unidecode(province).strip()
+            province_name_normalized =  unidecode(province_name).strip()
+            # Tạo một biểu thức chính quy để tìm kiếm toàn bộ tên tỉnh thành trong chuỗi
+            pattern = r"\b{}\b".format(province_normalized)
             if re.search(pattern, province_name_normalized.lower(), re.IGNORECASE):
                 return region
-            # Kiểm tra các từ có chữ cái đầu giống nhau trong tỉnh thành và trong danh sách
-            if all(word.lower() in province_normalized.lower().split() for word in province_name.split()):
-                return region
+          
     return None
+
 
 # Gán tên vùng cho từng tỉnh trong DataFrame
 df["Vùng"] = df["Huyện, Tỉnh"].map(check_province_name)
